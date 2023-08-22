edf11dbf6d955a26a529606070bd658fdb8542df
Hichem <78315377+CryptOran@users.noreply.github.com>
2023-08-22 09:15:30 +0000

    adding test

--- tests/test_App.py
+++ tests/test_App.py
@@ -1,2 +1,15 @@
-def test_app():
-    print(test 1 done)
+import pytest
+from src.App import add
+
+def test_add():
+    """Test the add function."""
+    
+    # Arrange: Set up any necessary data or preconditions
+    x = 5
+    y = 10
+    
+    # Act: Call the function/method you're testing
+    result = add(x, y)
+    
+    # Assert: Check the result against expected outcomes
+    assert result == 15, f"Expected {x + y}, but got {result}"
